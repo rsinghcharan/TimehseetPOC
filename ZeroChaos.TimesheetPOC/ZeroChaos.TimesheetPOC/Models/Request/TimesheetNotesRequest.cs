0146a1d9b5c3b28a4c152d285a5c9144217149e4
Rajveer Singh <rsingh@zerochaos.com>
2017-04-10 13:41:53 +0530

    Timesheet Notes Page

---
+++ ZeroChaos.TimesheetPOC/ZeroChaos.TimesheetPOC/Models/Request/TimesheetNotesRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Xamarin.Forms;
+using ZeroChaos.TimesheetPOC.Models;
+
+namespace ZeroChaos.TimesheetPOC
+{
+	/// <summary>
+	/// TimesheetNotesRequest class containes the required necessary properties for getting TimesheetNotes Data.
+	/// </summary>
+	public class TimesheetNotesRequest : ContentPage
+	{
+		public int Offset { get; set; }
+		public int Pagesize { get; set; }
+		public int objectID { get; set; }
+		public int clientID { get; set; }
+		public int PKID { get; set; }
+		/// <summary>
+		/// loggedonUser class contains the list of objects required to create the loggedonUser and in Which we will store the current user details.
+		/// </summary>
+		/// <value>The loggedon user.</value>
+		public UserInfo loggedonUser { get; set; }
+	}
+}
+
---
+++ ZeroChaos.TimesheetPOC/ZeroChaos.TimesheetPOC/Models/Response/TimesheetNotesResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using ZeroChaos.TimesheetPOC.Models.Response;
+
+namespace ZeroChaos.TimesheetPOC
+{
+
+	public class NoteType
+	{
+		public bool isVisible { get; set; }
+		public bool isModify { get; set; }
+		public bool isRequired { get; set; }
+		public string caption { get; set; }
+		public string fieldValue { get; set; }
+	}
+	public class ZcwNoteList
+	{
+		public string note { get; set; }
+		public NoteType noteType { get; set; }
+		public string emails { get; set; }
+		public string createdBy { get; set; }
+		public string createdOn { get; set; }
+		public string objectStatus { get; set; }
+		public int notesID { get; set; }
+		public bool serviceLevelAgreement { get; set; }
+	}
+	public class TimesheetNotesResponse : ResponseBase
+	{
+		public bool isNotesVisible { get; set; }
+		public bool isNoteTypeVisible { get; set; }							
+		public bool isEmailToVisible { get; set; }
+		public bool isCreatedByVisible { get; set; }
+		public bool isCreatedOnVisible { get; set; }
+		public bool isObjectStatusNameVisible { get; set; }
+		public IList<ZcwNoteList> zcwNoteList { get; set; }
+		public int recordCount { get; set; }
+	}
+}
+
