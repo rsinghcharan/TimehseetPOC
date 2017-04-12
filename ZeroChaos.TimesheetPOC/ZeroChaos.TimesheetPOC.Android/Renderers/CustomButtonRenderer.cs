f3a8dad2a5ade402935dcfd9ad28189e2e370d81
Pragnesh Patel <ppatel2@zerochaos.com>
2017-04-12 12:24:45 +0530

    gradient Button

--- ZeroChaos.TimesheetPOC/ZeroChaos.TimesheetPOC.Android/Renderers/CustomButtonRenderer.cs
+++ ZeroChaos.TimesheetPOC/ZeroChaos.TimesheetPOC.Android/Renderers/CustomButtonRenderer.cs
@@ -11,49 +11,49 @@ using Xamarin.Forms;
 using ZeroChaos.TimesheetPOC.Controls;
 using ZeroChaos.TimesheetPOC.Droid.Renderers;
 
-
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
 namespace ZeroChaos.TimesheetPOC.Droid.Renderers
 {
-	public class CustomButtonRenderer : ButtonRenderer
+	public class CustomButtonRenderer:ButtonRenderer
 	{
 		public CustomButtonRenderer()
 		{
+			
+		} 
+		  
+				protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+				{
+					base.OnElementChanged(e);
 
-		}
+					var elementButton = this.Element as CustomButton;
+						//var button = this.Control;
+						//button.SetAllCaps(false);
 
-		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
-		{
-			base.OnElementChanged(e);
+						Android.Widget.Button thisButton = Control as Android.Widget.Button;
+						thisButton.SetAllCaps(false);
+						thisButton.Touch += (object sender, Android.Views.View.TouchEventArgs e2) =>
+						            {
+										if (e2.Event.Action == MotionEventActions.Down)
+										{
+											System.Diagnostics.Debug.WriteLine("TouchDownEvent");
+					                 // Toast.MakeText(this, "Key Up", ToastLength.Short).Show();
 
-			var elementButton = this.Element as CustomButton;
-			//var button = this.Control;
-			//button.SetAllCaps(false);
+										}
+										else if (e2.Event.Action == MotionEventActions.Up)
+										{
+											
 
-			Android.Widget.Button thisButton = Control as Android.Widget.Button;
-			thisButton.SetAllCaps(false);
-			thisButton.Touch += (object sender, Android.Views.View.TouchEventArgs e2) =>
-						{
-							if (e2.Event.Action == MotionEventActions.Down)
-							{
-								System.Diagnostics.Debug.WriteLine("TouchDownEvent");
-								// Toast.MakeText(this, "Key Up", ToastLength.Short).Show();
-
-							}
-							else if (e2.Event.Action == MotionEventActions.Up)
-							{
-
-
-								System.Diagnostics.Debug.WriteLine("TouchUpEvent");
-								Control.CallOnClick();
-							}
-						};
-			// CustomButton.SetAllCaps(false);
-			if (elementButton.Type == "Orange")
-				Control.SetBackgroundDrawable(CreateGradientColor("#ffb85c", "#d25903", "#FFF6923F"));
-			else if (elementButton.Type == "Blue")
-				Control.SetBackgroundDrawable(CreateGradientColor("#3c9ece", "#01446b", "#FFB95454"));
-			else Control.SetBackgroundDrawable(CreateGradientColor("#3c9ece", "#01446b", "#FF98B954"));
-		}
+											System.Diagnostics.Debug.WriteLine("TouchUpEvent");
+											Control.CallOnClick();
+										}
+						            };
+			      // CustomButton.SetAllCaps(false);
+					if (elementButton.Type == "Orange")
+						Control.SetBackgroundDrawable(CreateGradientColor("#ffb85c", "#d25903", "#FFF6923F"));
+					else if (elementButton.Type == "Blue")
+						Control.SetBackgroundDrawable(CreateGradientColor("#3c9ece", "#01446b", "#FFB95454"));
+					else Control.SetBackgroundDrawable(CreateGradientColor("#3c9ece", "#01446b", "#FF98B954"));
+				}
 
 		private GradientDrawable CreateGradientColor(string color1, string color2, string borderColor)
 		{
@@ -63,6 +63,6 @@ namespace ZeroChaos.TimesheetPOC.Droid.Renderers
 			//gd.SetCornerRadius(0f);
 			//gd.SetStroke(5, Color.FromHex(borderColor).ToAndroid());
 			return gd;
-		}
-	}
+			}
+					}
 }
