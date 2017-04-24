290830662e1e61959195654295a6dd30e29c92f6
Gaurav Makwana <gmakwana@ZC-Maints-MacBook-Pro-7.local>
2017-04-24 12:10:42 +0530

    Engagement spend summary

---
+++ ZeroChaos.TimesheetPOC/ZeroChaos.TimesheetPOC/Views/Engagement/SpendSummaryPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ZeroChaos.TimesheetPOC
+{
+	public partial class SpendSummaryPage : ContentPage
+	{
+		public SpendSummaryPage()
+		{
+			InitializeComponent();
+			EngagementSpendSummaryViewModel cvm = new EngagementSpendSummaryViewModel();
+			this.BindingContext = cvm;
+		}
+	}
+}
