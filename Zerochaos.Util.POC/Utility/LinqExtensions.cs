a3bf3cc490c1f021d982d602486c1d7505232af7
ZEROCHAOS\dpatel2 <dpatel2@zerochao.com>
2017-04-25 14:12:08 +0530

    Added Utility method

---
+++ Zerochaos.Util.POC/Utility/LinqExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zerochaos.Util.POC.Utility
+{
+    public static class LinqExtensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> _LinqResult)
+        {
+            return new ObservableCollection<T>(_LinqResult);
+        }
+        public static int IndexOf(this IEnumerable self, object obj)
+        {
+            int index = -1;
+
+            var enumerator = self.GetEnumerator();
+            enumerator.Reset();
+            int i = 0;
+            while (enumerator.MoveNext())
+            {
+                if (enumerator.Current == obj)
+                {
+                    index = i;
+                    break;
+                }
+
+                i++;
+            }
+
+            return index;
+        }
+    }
+}
