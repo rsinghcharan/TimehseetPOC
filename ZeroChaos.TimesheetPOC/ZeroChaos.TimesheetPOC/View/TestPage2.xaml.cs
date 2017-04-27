09bf63790e616e5f3ca861ee096110312c4043d8
oteredesaizc <oteredesai@zerochaos.com>
2017-04-27 14:57:38 +0530

    fixed the view namespace error.

--- ZeroChaos.TimesheetPOC/ZeroChaos.TimesheetPOC/View/TestPage2.xaml.cs
+++ ZeroChaos.TimesheetPOC/ZeroChaos.TimesheetPOC/View/TestPage2.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace ZeroChaos.TimesheetPOC.View
+namespace ZeroChaos.TimesheetPOC.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TestPage2 : ContentPage
