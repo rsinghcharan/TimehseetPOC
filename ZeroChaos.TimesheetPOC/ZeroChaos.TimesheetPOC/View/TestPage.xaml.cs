7b36ce5151061279cd6b1005723f8ecc177eff03
Gaurav Makwana <gmakwana@ZC-Maints-MacBook-Pro-7.local>
2017-04-27 12:34:59 +0530

    Checking cs log

---
+++ ZeroChaos.TimesheetPOC/ZeroChaos.TimesheetPOC/View/TestPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ZeroChaos.TimesheetPOC
+{
+	public partial class TestPage : ContentPage
+	{
+		public TestPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
